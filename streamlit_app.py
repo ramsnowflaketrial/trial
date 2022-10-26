9f7ff710ce6875f061c0676d722a5c26b4b5f4dc
ramsnowflaketrial <116730914+ramsnowflaketrial@users.noreply.github.com>
2022-10-26 21:11:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.title('My parents are now healthy diner')
+streamlit.header('Breakfast Favourites')
+streamlit.text('🍌Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥭Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥝Hard-Boiled Free-Range Egg')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
