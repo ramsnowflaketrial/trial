1f313b40ade9206e04afc74c04eba65024998fb9
ramsnowflaketrial <116730914+ramsnowflaketrial@users.noreply.github.com>
2022-11-01 14:07:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 streamlit.title('My parents are now healthy diner')
 streamlit.header('Breakfast Favourites')
 streamlit.text('🍌Omega 3 & Blueberry Oatmeal')
@@ -27,6 +28,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
