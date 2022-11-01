ba8dad67d01024431f6c5b81475da231519636dd
ramsnowflaketrial <116730914+ramsnowflaketrial@users.noreply.github.com>
2022-11-01 13:55:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,6 @@ streamlit.dataframe(my_data_row)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('The user entered ', add_my_fruit)
-my_cnx.cursor().execute("Insert into FRUIT_LOAD_LIST values (add_my_fruit)")
+my_cnx.cursor().execute("Insert into FRUIT_LOAD_LIST" "values (?)", (add_my_fruit))
 
 
